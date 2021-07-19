554e1ae7c933cc512af183fe056ca1ed7b07a07d
lucassherwin <lksherwin@gmail.com>
2021-07-19 14:33:38 -0400

    adds comments

--- app/javascript/graphqlProvider.tsx
+++ app/javascript/graphqlProvider.tsx
@@ -10,11 +10,11 @@ const csrfToken = document
   .querySelector('meta[name=csrf-token]')
   .getAttribute('content')
 
-  const client = new ApolloClient({
-  link: new HttpLink({
+  const client = new ApolloClient({ // client is how Apollo is configured to connect to the api
+  link: new HttpLink({ // makes requests to /graphql
     credentials: 'same-origin',
     headers: {
-      'X-CSRF-Token': csrfToken,
+      'X-CSRF-Token': csrfToken, // prevents users from making requests through our graphql api
     },
   }),
   cache: new InMemoryCache(),
@@ -24,7 +24,7 @@ export const withProvider = (
   WrappedComponent: React.ComponentType,
   props: any = {},
   ) => () => {
-  return (
+  return ( // this allows the WrappedComponent to make calls to the graphql api
     <ApolloProvider client={client}>
       <WrappedComponent {...props} />
     </ApolloProvider>
